ac1959150e2e01e617755b18dbddd18f89d5c50f
shrutikawani <shrutika.daripadhar@eduonix.com>
2015-09-28 15:01:38 +0530

    0.2.0 release

---
+++ jquery.linkIt.js
@@ -0,0 +1,35 @@
+/*
+* name :	 LinkIt
+* author:	 Shrutika
+* version:	 0.1.0
+* license:	 MIT 
+*/
+
+(function ($){
+	$.fn.linkIt = function(options){
+		//Default Settings
+		var settings = $.extend({
+			href :null,
+			text :null,
+			target:'_self'
+			
+		}, options);
+	
+		//validation
+		if(settings.href == null){
+			console.log('You need an href option for linkIt to work');
+			return this;
+		}
+		
+		return this.each(function(){
+			var object = $(this);
+			
+			if(settings.text == null){
+				settings.text = object.text();
+			}
+					object.wrap('<a target="'+settings.target+'" href="'+settings.href+'">').text(settings.text);
+		});
+		
+
+	}
+}(jQuery));
